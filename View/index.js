e52fcc3b3a9402729368aaf32fe4e3c11d0b3a0f
MinhTri <str.comparetoignore@gmail.com>
2023-11-11 20:32:48 +0700

    add

--- View/index.js
+++ View/index.js
@@ -6,9 +6,17 @@ dotenv.config()
 const PORT = process.env.PORT||3000
 const Main = express()
 
-
+Main.use(cors())
 Main.use(registerRouter)
 Main.use(login)
+
+Main.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:19006');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+  });
+
 Main.listen(PORT,()=>{
     console.log(`App is running on port ${PORT}`);
 })
