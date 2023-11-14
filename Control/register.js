d2eb70cb4a68700b692a03174ebdee9595b9743b
MinhTri <str.comparetoignore@gmail.com>
2023-11-15 01:02:32 +0700

    cors register

--- Control/register.js
+++ Control/register.js
@@ -1,17 +1,16 @@
 import express, { json } from "express"
 import { CreateUserAccount } from "../Model/ConnectToDb.js"
 import bcrypt from 'bcryptjs'
+import cors from 'cors'
 export const registerRouter = express.Router()
 
+registerRouter.use(cors())
 registerRouter.use(express.json())
 
 registerRouter.post("/Register", async (req, res, next)=>{
-
-
-    console.log(req.body)
     const salt = await bcrypt.genSalt(10);
     const password  = await bcrypt.hash(req.body.password, salt)
     await CreateUserAccount({"username":req.body.username,"password":password})
     res.send("done")
-})
+}
 
