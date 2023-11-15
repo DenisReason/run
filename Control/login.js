b671b42a73f9d8acea7e333e3931e361722a7c50
MinhTri <str.comparetoignore@gmail.com>
2023-11-15 23:48:59 +0700

    fix1

--- Control/login.js
+++ Control/login.js
@@ -6,8 +6,9 @@ export const login = express.Router()
 
 login.post("/login", async(req, res, next)=>{
     const User = await GetuserByUsername(req.body.username)
-    if(User){
-        
+    if(!User){
+        res.status(401).send("Account does not exist")
+        return
     }
     const Result = await bcrypt.compare(req.body.password, User.password)
     if(Result){
