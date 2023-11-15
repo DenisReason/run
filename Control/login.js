52fd32dfcfdd59f23da1faf8cd57a13a6be1f4aa
MinhTri <str.comparetoignore@gmail.com>
2023-11-15 23:30:53 +0700

    add token

--- Control/login.js
+++ Control/login.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import { GetuserByUsername } from '../Model/ConnectToDb.js'
+import { genToken } from '../Model/jwt.js'
 export const login = express.Router()
 
 login.post("/login", async(req, res, next)=>{
     const User = await GetuserByUsername(req.body.username)
-    console.log(User);
+    if(User){
+        
+    }
     const Result = await bcrypt.compare(req.body.password, User.password)
-    console.log(Result)
     if(Result){
-        res.send(User)
+        const token = genToken(Result)
+        res.status(200).send(token)
     }
     else{
         res.send("Incorrect account name or password")
