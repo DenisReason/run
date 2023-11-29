7b44f7aa2869002ec3e5961bcf1ed8e779248b9f
MinhTri <str.comparetoignore@gmail.com>
2023-11-29 22:21:22 +0700

    add check token

--- Model/jwt.js
+++ Model/jwt.js
@@ -4,11 +4,25 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 const secretKey = process.env.KEY
-export const genToken = async (req, res, next)=>{
+export const genToken = async (req, res, next) => {
     const payload = {
-        "username":req.body.username,
-        "password":req.body.password
+        "username": req.body.username,
+        "password": req.body.password
     }
-    const token = await jwt.sign(payload, secretKey, {expiresIn:"30s"})
+    const token = await jwt.sign(payload, secretKey, { expiresIn: "30s" })
     return token
+}
+
+export const Checktoken = async (token) => {
+    try {
+        // Sử dụng hàm verify từ thư viện jsonwebtoken
+        const decodedToken =  await jwt.verify(token, secretKey);
+
+        // Trả về đối tượng chứa thông tin xác minh
+        return decodedToken;
+    } catch (error) {
+        // Xử lý lỗi nếu có
+        console.error('Lỗi xác minh token:', error);
+        return null;
+    }
 }
--- View/index.js
+++ View/index.js
@@ -3,6 +3,7 @@ import {registerRouter} from '../Control/register.js';
 import { login } from '../Control/login.js';
 import dotenv from 'dotenv'
 import cors from 'cors'
+import { Checktoken } from '../Model/jwt.js';
 dotenv.config()
 const PORT = process.env.PORT||3000
 const Main = express()
@@ -10,6 +11,8 @@ const Main = express()
 Main.use(cors())
 Main.use(registerRouter)
 Main.use(login)
+Main.post("/", Checktoken)
+
 
 Main.listen(PORT,()=>{
     console.log(`App is running on port ${PORT}`);
