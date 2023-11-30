20b76651e3b8dbdb04ddffbdec56cbbe3c9cc7e2
MinhTri <str.comparetoignore@gmail.com>
2023-11-30 20:51:29 +0700

    add

--- Control/logintoken.js
+++ Control/logintoken.js
@@ -2,6 +2,4 @@ import express from 'express'
 import { Checktoken } from '../Model/jwt.js'
 export const LoginToken = express.Router()
 
-LoginToken.post('/', async (req, res, next)=>{
-    await Checktoken(req, res, next)
-})
+LoginToken.post('/', Checktoken)
