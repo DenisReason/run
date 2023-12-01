f924b447f337e13b30e7449e212dd9eef0ef3637
MinhTri <str.comparetoignore@gmail.com>
2023-12-01 12:29:35 +0700

    state

--- Control/login.js
+++ Control/login.js
@@ -5,34 +5,37 @@ import { Checktoken, genToken } from '../Model/jwt.js'
 export const login = express.Router()
 login.use(express.json())
 
-login.post("/login", async(req, res, next)=>{
+login.post("/login", async (req, res, next) => {
     console.log('====================================');
     console.log(req.body.token);
     console.log('====================================');
-    if(req.body.token){
+    if (req.body.token) {
         const token = req.body.token
         const state = Checktoken(token)
-        res.send("state: ",state)
+        res.status(200).send("state: ", state)
         // if(state!=null){
         //     console.log("finish");
         //     res.status(200).send("oke step 1: ",state)
         // }
         return
     }
-    const User = await GetuserByUsername(req.body.username)
-    console.log(User);
-    if(!User){
-        res.status(401).send("Account does not exist")
-        return
+    else {
+        const User = await GetuserByUsername(req.body.username)
+        console.log(User);
+        if (!User) {
+            res.status(401).send("Account does not exist")
+            return
+        }
+        const Result = await bcrypt.compare(req.body.password, User.password)
+        console.log("Result:", Result);
+        if (Result) {
+            const token = await genToken(req, res, next)
+            res.send(token)
+        }
+        else {
+            res.send("Incorrect account name or password")
+        }
     }
-    const Result = await bcrypt.compare(req.body.password, User.password)
-    console.log("Result:", Result);
-    if(Result){
-        const token = await genToken(req, res, next)
-        res.send(token)
-    }
-    else{
-        res.send("Incorrect account name or password")
-    }
+
 })
 
